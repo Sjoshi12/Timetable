3da925cc7b5e8f1ae2ad93ea5a9ee38190b422db
vinod s <vinod s@156.59.50.47>
2017-08-06 08:11:47 +1200

    commit in student page

--- Assignment1/src/login.java
+++ Assignment1/src/login.java
@@ -1,18 +1,10 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import java.awt.Font;
-import javax.swing.JButton;
-import javax.swing.JTextArea;
 
 public class login {
 
 	private JFrame frame;
-	private JTextField txtPassword;
-	private JTextArea textArea;
-	private JTextArea textArea_1;
 
 	/**
 	 * Launch the application.
@@ -44,29 +36,6 @@ public class login {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("Username");
-		lblNewLabel.setFont(new Font("Monospaced", Font.PLAIN, 13));
-		lblNewLabel.setBounds(57, 57, 80, 20);
-		frame.getContentPane().add(lblNewLabel);
-		
-		txtPassword = new JTextField();
-		txtPassword.setText("Password");
-		txtPassword.setBounds(51, 100, 86, 20);
-		frame.getContentPane().add(txtPassword);
-		txtPassword.setColumns(10);
-		
-		JButton btnLogin = new JButton("Login");
-		btnLogin.setBounds(159, 181, 89, 23);
-		frame.getContentPane().add(btnLogin);
-		
-		textArea = new JTextArea();
-		textArea.setBounds(216, 56, 80, 22);
-		frame.getContentPane().add(textArea);
-		
-		textArea_1 = new JTextArea();
-		textArea_1.setBounds(216, 98, 80, 22);
-		frame.getContentPane().add(textArea_1);
 	}
+
 }
