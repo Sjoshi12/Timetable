e2d59533bfd3da698a4dfc85c1b20234a84beef0
vinod s <vinod s@192.168.1.5>
2017-08-06 08:46:20 +1200

    login

--- Assignment1/src/login.java
+++ Assignment1/src/login.java
@@ -1,6 +1,13 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class login {
 
@@ -36,6 +43,33 @@ public class login {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("user name");
+		lblNewLabel.setFont(new Font("Monospaced", Font.PLAIN, 20));
+		lblNewLabel.setBounds(34, 27, 115, 44);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("password");
+		lblNewLabel_1.setFont(new Font("Monospaced", Font.PLAIN, 20));
+		lblNewLabel_1.setBounds(44, 87, 125, 44);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(216, 41, 103, 30);
+		frame.getContentPane().add(textArea);
+		
+		JTextArea textArea_1 = new JTextArea();
+		textArea_1.setBounds(216, 101, 108, 30);
+		frame.getContentPane().add(textArea_1);
+		
+		JButton btnNewButton = new JButton("login");
+		btnNewButton.setFont(new Font("Monospaced", Font.PLAIN, 20));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(127, 165, 141, 39);
+		frame.getContentPane().add(btnNewButton);
 	}
-
 }
